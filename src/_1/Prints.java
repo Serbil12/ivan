e46ca814dd0a6fcf06382d7649547808d06284ef
Nagovitcyn Sergey <nagovitcyn-sa@dartit.ru>
2022-11-15 14:44:27 +0300

    first commit

---
+++ src/_1/Prints.java
@@ -0,0 +1,11 @@
+package _1;
+
+
+public class Prints {
+    public static void main(String[] args) {
+        System.out.print("Hello world!"); // без переноса строки
+        System.out.println("Hello!"); // с переносом строки после вывода сообщения
+
+        System.out.println(10); // выводить можно как числа, так и строки
+    }
+}
