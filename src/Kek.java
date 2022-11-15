85ad3ac221a9ca9a52c87391819df8a42d4b015d
Nagovitcyn Sergey <nagovitcyn-sa@dartit.ru>
2022-11-15 19:21:07 +0300

    Первое занятие

--- src/Kek.java
+++ src/Kek.java
@@ -1,9 +1,23 @@
 // main, вывод данных
-public class Prints {
+public class Kek {
     public static void main(String[] args) {
         System.out.print("Hello world!"); // без переноса строки
         System.out.println("Hello!"); // с переносом строки после вывода сообщения
-
         System.out.println(10); // выводить можно как числа, так и строки
+
+        // однострочные комментарии
+        // dajsdsakjndsajknadsjk
+        // osdjoasd
+        // oasdjohads
+
+        // многострочные комментарии
+        /*
+        asduhaiusdhids
+        adsoihasdiuhiasd
+        adsohasduohasd
+        oaisdhoiuasdhou
+
+        aodisoiasdhoiahdso
+         */
     }
 }
