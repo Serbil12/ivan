8e7f1485b7727a74069103c0999875e1365f73d2
Nagovitcyn Sergey <nagovitcyn-sa@dartit.ru>
2022-11-18 23:45:34 +0300

    task

--- code/ВводДанных_Условия/Constans.java
+++ code/ВводДанных_Условия/Constans.java
@@ -12,8 +12,9 @@ import java.util.Scanner;
  */
 public class Constans {
     public static void main(String[] args) {
+        // ключевое слово final означает, что переменная является константой
+        // и ее значение можно задать только один раз и менять его больше нельзя
         final Scanner sc = new Scanner(System.in);
         final double PI = 3.14; // постоянное число
-        // не меняется в программе
     }
 }
